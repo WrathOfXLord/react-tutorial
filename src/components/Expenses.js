69fe4a2bf4348bcc5e768d075b2d8c0b7dd11fd9
alifirato <alifirato@gmail.com>
2023-04-07 01:01:11 +0300

    better way to place the data

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,12 +2,11 @@ import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(propData) {
-    const expenses = propData.expenses;
+    const expenses = propData.expenses.map(element => {
+        return <ExpenseItem title={element.title} amount={element.amount} date={element.date}></ExpenseItem>
+    });;
     return (
-        <div className="expenses">
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-        </div>
+        <div className="expenses">{expenses}</div>
     );
 }
 

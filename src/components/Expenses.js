82468d47a4046beaa9e313c58e349aa0a859698a
alifirato <alifirato@gmail.com>
2023-04-07 09:26:31 +0300

    redundant semicolon

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,7 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 function Expenses(propData) {
     const expenses = propData.expenses.map(element => {
         return <ExpenseItem title={element.title} amount={element.amount} date={element.date}></ExpenseItem>
-    });;
+    });
     return (
         <div className="expenses">{expenses}</div>
     );
